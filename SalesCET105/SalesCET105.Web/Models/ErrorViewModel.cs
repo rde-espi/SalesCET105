98c1e5a742ba12cefd2a32109a155fac5ca22dd7
rde-espi <reinaldo_7531@hotmail.com>
2026-07-31 10:00:36 +0100

    init web project

---
+++ SalesCET105/SalesCET105.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesCET105.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
